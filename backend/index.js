d814c86e8579debecffaa0a43995805b4e0a2b3f
fenilsheladiya <fenilsheladiya033@gmail.com>
2025-02-08 13:24:33 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import dotenv from 'dotenv'
+import dbConnect from "./DB/dbConnect.js";
+import authRouter from  './rout/authUser.js'
+import messageRouter from './rout/messageRout.js'
+import userRouter from './rout/userRout.js'
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
+
+import {app , server} from './Socket/socket.js'
+
+const __dirname = path.resolve();
+
+dotenv.config();
+
+
+app.use(express.json());
+app.use(cookieParser())
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
+
+app.use('/api/auth',authRouter)
+app.use('/api/message',messageRouter)
+app.use('/api/user',userRouter)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+    dbConnect();
+    console.log(`Working at ${PORT}`);
+})
